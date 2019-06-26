c98267107f7b236a73b41890066d0ee0ee83b3b9
Gerardo Keys <jotojry@icloud.com>
2019-06-25 18:52:05 -0700

    Updates

---
+++ precache-manifest.4636afeff4f32fae9f4b60d41a18df1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "64696d5a14bdb785a063",
+    "url": "/course_directory/static/js/runtime~main.64696d5a.js"
+  },
+  {
+    "revision": "ddd845617a9bae0aa973",
+    "url": "/course_directory/static/js/main.ddd84561.chunk.js"
+  },
+  {
+    "revision": "f5ae2494a8dbd9f95f4b",
+    "url": "/course_directory/static/js/1.f5ae2494.chunk.js"
+  },
+  {
+    "revision": "ddd845617a9bae0aa973",
+    "url": "/course_directory/static/css/main.2c9826fb.chunk.css"
+  },
+  {
+    "revision": "55ffb86959b0c8664f446b689119b228",
+    "url": "/course_directory/index.html"
+  }
+];
